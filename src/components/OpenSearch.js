edd626ad8b9174ad7a5e6ba649c1ae0e2cf0f887
Mayowa Adediran <ddrn_mayowa@yahoo.com>
2019-06-13 20:07:35 +0100

    link search page

--- src/components/OpenSearch.js
+++ src/components/OpenSearch.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 const OpenSearch = () => {
   return (
     <div className="open-search">
-      <button onClick={() => this.setState({ showSearchPage: true })}>Add a book</button>
+      <Link className="btn-search" to="/search">Add a book</Link>
     </div>
   )
 }
