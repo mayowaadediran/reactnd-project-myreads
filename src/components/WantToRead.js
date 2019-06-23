72f8509a7aad24b5b66bc2e1b258b7dcddc95f64
Mayowa Adediran <ddrn_mayowa@yahoo.com>
2019-06-23 10:30:28 +0100

    filter wanttoread books

--- src/components/WantToRead.js
+++ src/components/WantToRead.js
@@ -1,17 +1,28 @@
 import React from 'react'
 import Book from './Book'
 
-export default function WantToRead(props) {
+ function WantToRead(props) {
+
+  const read = props.books.filter((book) => {
+    return book.shelf === "wantToRead"
+  })
   return (
      <div className="bookshelf">
         <h2 className="bookshelf-title">Want to Read</h2>
         <div className="bookshelf-books">
           <ol className="books-grid">
-      
-              <Book />
-        
+            {read.map((book) => (
+              <Book 
+                 key={book.id}
+                title={book.title}
+                image={book.imageLinks.thumbnail}
+              />
+            ))}
+              
           </ol>
         </div>
       </div>
   )
 }
+
+export default WantToRead
