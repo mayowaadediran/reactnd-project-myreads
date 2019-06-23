ec526ed0d74b652aa2eff08c222859dbc8e7a503
Mayowa Adediran <ddrn_mayowa@yahoo.com>
2019-06-23 10:30:00 +0100

    filter read books

--- src/components/Read.js
+++ src/components/Read.js
@@ -1,17 +1,29 @@
 import React from 'react'
 import Book from './Book'; 
 
-export default function Read(props) {
+const Read = (props) => {
+
+    const read = props.books.filter((book) => {
+      return book.shelf === "read"
+    })
   return (
      <div className="bookshelf">
         <h2 className="bookshelf-title">Read</h2>
         <div className="bookshelf-books">
           <ol className="books-grid">
-
-              <Book />
-           
+            {read.map((book) => {
+              return (
+                <Book 
+                  key={book.id}
+                  title={book.title}
+                  image={book.imageLinks.thumbnail}
+                />
+              )
+            })}
           </ol>
         </div>
       </div>
   )
 }
+
+export default Read 
